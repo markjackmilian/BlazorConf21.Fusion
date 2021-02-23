701a529fc957172d8d06818d42e834d1ae25b375
Marco <emme.milani@hotmail.it>
2021-02-23 20:06:17 +0100

    add component for group subscription

--- BlazorConf21.Fusion/Fusion.ServerSide/Services/WeatherForecastService.cs
+++ BlazorConf21.Fusion/Fusion.ServerSide/Services/WeatherForecastService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Fusion.ServerSide.Data;
+using Stl.Async;
 using Stl.Fusion;
 
 namespace Fusion.ServerSide.Services
@@ -24,6 +25,28 @@ namespace Fusion.ServerSide.Services
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToArray());
+        }
+        
+        [ComputeMethod]
+        public virtual Task<WeatherForecast[]> GetForecastAsync(string city)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Today.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+
+        public Task UpdateForecastForCity(string city)
+        {
+            using (Computed.Invalidate())
+            {
+                this.GetForecastAsync(city).Ignore();
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
